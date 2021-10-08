b0b2919619e590f5ff2a12df4e8377b0f15aef78
Oscar Salas Miranda <73209356+oscarsalas0392@users.noreply.github.com>
2021-10-07 21:05:43 -0600

    Clase 2 completa

---
+++ src/CustomersApi/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+WebHost.CreateDefaultBuilder().Configure(app =>
+{
+    app.UseRouting();
+    app.UseEndpoints(e =>
+    {
+        e.MapGet("/", c => c.Response.WriteAsync("Hello world!"));
+        e.MapGet("hello/{name}", c => c.Response.WriteAsync($"Hello, {c.Request.RouteValues["name"]}"));
+    });
+}).Build().Run();
